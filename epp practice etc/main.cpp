b775d32504e6e139430d1bf28f26f094b0b5bdc7
minavadamee20 <34077203+minavadamee20@users.noreply.github.com>
2018-12-11 10:36:16 -0800

    finishing practice5
    the last issue with this function is that i want to return the vector, but since i've made a new vector on the heap, i do not know how to return the entire vector that is on the heap.
    
    so far i am returning the first elemnet within the vector. need to figure out how to return a  new vector that is made on the heap back to main and then need to figure out how to delete said new vector that was made on the heap.
    
    then i will be completely done with practice 5 vector and move onto the example prompts that were given on csuf website

--- epp practice etc/main.cpp
+++ epp practice etc/main.cpp
@@ -65,13 +65,13 @@ void practice3(int sample_array[]) {
 			if (*ptr > *next) {
 				temp = *ptr;
 				*ptr = *next;
-				*next = temp;
+				*next = temp;		//a better version of the above algorithm is std::swap (*ptr, *next); and it works all the same
 				ptr++; next++;
+				
 			}
 			else { ptr++; next++; }
 		}
 	}
-
 	next = sample_array;
 	std::cout << "the new array is: [ ";
 	for (int readloop = 0; readloop < sizeof(sample_array)+1; readloop++) {
@@ -81,25 +81,125 @@ void practice3(int sample_array[]) {
 	std::cout << "]" << std::endl;
 	return;
 }
-/*the problem: the problem that I had a pointer that would point to the beginning of the array (*temp). and everytime I would use temp to store the temporarily store the value, it would point to the first value within the array
+/*the problem: the problem was that I had a pointer that would point to the beginning of the array (*temp). and everytime I would use temp to store the temporarily store the value, it would point to the first value within the array
 because i was not also increasing temp along with the function. 
-	Now I could've also just simply also increased temp with the function, but a simple fix was just to make temp an int holder*/
+	Now I could've also just simply also increased temp with the function, but a simple fix was just to make temp an int holder rather than a int pointer*/
 
 
-//in practice 4, we'll be asking for an array to be input, meaning the array can be as large as the user wants it to be, and then we'll use the above sort algorithm to find the largest and smallest item within the array. 
-void practice4() {
-	int array_size;
+
+
+
+
+
+
+
+
+
+
+//
+////for practice 3 we'll be doing the recursions functions using the practice 3 algorithm, in practice 5 we will try to return a new int array that has been made on the heap
+//void practice4(
+//	int sample_array[], 
+//	int firstcounter, 
+//	int secondcounter, 
+//	int *ptr,
+//	int *next) {	//remember: that in our last function we used the size of the array to know when to stop the array. so we can do that in this case as well.
+//														//in this case however we will have to pass a num that will essentially be firstloop variable that was delcared in the second loop, and we'll also pass another variable
+//														//that'll act as the first variable x in the first loop function
+//
+//
+//	int temp_value_holder;	//this is to temporarily store the int that is located in *ptr or *next_ptr. 
+//
+//	//first we need our base case, that says if the array is empty, then we'll return back to the function 
+//	if (firstcounter == 0) {
+//		ptr = sample_array;
+//		std::cout << std::endl << std::endl << "\t\t\t the contents of the new array are: [ ";
+//		for (int p = 0; p < sizeof(sample_array) + 1; p++) {
+//			std::cout << *ptr << " "; ptr++;
+//		}
+//		std::cout << " ]" << std::endl;
+//		return;
+//	}
+//	//another case that says if first counter reaches 0 we will return back to the main function 
+//	if (secondcounter == 0) {
+//		practice4(sample_array, firstcounter--, secondcounter = sizeof(sample_array), ptr = &sample_array[0], next = ptr++);
+//	}
+//	if (*ptr > *next) {
+//		temp_value_holder = *ptr;
+//		*ptr = *next;
+//		*next = temp_value_holder;
+//		practice4(sample_array, firstcounter, secondcounter--, ptr++, next++);
+//		/*ptr++; next++;
+//		secondcounter--;*/
+//	}
+//	else 
+//		practice4(sample_array, firstcounter, secondcounter--, ptr++, next++);
+//
+//}
+
+
+
+
+
+
+//in practice 4 we'll be using vectors to help further understand vectors, then in the next practice we'll cover recursions using vectors again
+std::vector<int> practice4(std::vector<int> v1) {
+
+	std::cout << "\t\t input your values, end your input by inputting a number below 0: ";
 	int values;
-	std::cout << "enter values: ";
-	int element_location = 0;
-	while (values > 0) {
-		std::cin >> values;
+	while(std::cin>>values && values >= 0){
+		v1.push_back(values);
 	}
-	std::cout << "last value: " << values;
+	std::vector<int>::iterator it;
+	std::cout << std::endl << "\t end of inputting values." << std::endl << " \t\t new vector that has been created reads: ";
+	for (it = v1.begin(); it < v1.end(); it++) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl << std::endl;
 
-	
-	
+	return v1;
 }
+//now let's try using the least to greatest algorithm that we obtained in practice 3
+std::vector<int> practice5(std::vector<int> v2) {
+	std::vector<int>* swap_pointer = new std::vector<int>(v2);
+	
+	int x = 0;
+	std::vector<int>::iterator it = v2.begin();
+	std::vector<int>::iterator next_it = v2.begin() + 1;
+	std::vector<int>::iterator first_loop = v2.begin();
+	
+	/*check notes about what is wrong with the below function within the code*/
+
+	it = swap_pointer->begin();
+	
+	for (first_loop = v2.begin(); first_loop < v2.end(); first_loop++) {	//can this be changed so that it's swap_pointer?
+		it = swap_pointer->begin();	//when looping back the first time, the pointer points to the end of the list and the second pointer points to the first element....why?
+		next_it = swap_pointer->begin() + 1;
+		x = 0;
+		for (it = swap_pointer->begin(); it < swap_pointer->end(); it++) {
+			
+			if (next_it >= swap_pointer->end()) {	//this says that if ht next_pointer is equal to 
+				break;
+			}
+			if (*it>*next_it) {
+				std::iter_swap(swap_pointer->begin()+ x, swap_pointer->begin()+ x + 1);
+				next_it++;
+				x++;
+			}
+			else { next_it++; x++; }
+		}
+	}
+
+	std::cout << std::endl << std::endl << "\t\t the new sorted vector looks like: [ ";
+	for (it = swap_pointer->begin(); it < swap_pointer->end(); it++) {
+		std::cout << *it << " ";
+	}
+	std::cout << " ]" << std::endl << std::endl << "\t\t we have reached the end of practice 5" << std::endl;
+
+	return *swap_pointer;	//this only returns the first element within the vector, i want to return the entire new vector
+}
+
+
 
 
 int main() {
@@ -110,12 +210,24 @@ int main() {
 	std::string sample_string = "Hello there!";
 	practice2(sample_string);
 
-	int some_array[100] = { 0,5,9,2,1 };
+	int some_array[5] = { 0,5,9,2,1 };
 	practice3(some_array);
 
-	practice4();
+	/*int &ptr = some_array[0];
+	int &next = some_array[1];*/
+	//practice4(some_array, sizeof(some_array), sizeof(some_array)+1, &ptr, &next);	//for practie 4 we want to cover vectors quickly
 
-	
+	std::vector<int> sample_vector;	//initialized the vector to hold integers and be called sample_vector
+	sample_vector = practice4(sample_vector);
+
+
+
+	//to-do: SUNDAY
+		//make recursions for practice 3
+		//go over parsing through a file
+	//to-do: MONDAY
+		//epp practice online
+	practice5(sample_vector);
 	
 
 
